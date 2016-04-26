a98e5ee04df919d08825370e30ad51b8654f8baf
sd1990 <songdan_obj@126.com>
2016-04-26 09:44:11 +0800

    add Generic Class Example Stack and StackNoGeneric

---
+++ src/com/songdan/demo/generics/Stack.java
@@ -0,0 +1,9 @@
+package com.songdan.demo.generics;
+
+/**
+ * @author Songdan
+ * @date 2016/4/26
+ */
+public class Stack {
+
+}
---
+++ src/com/songdan/demo/generics/StackNoGeneric.java
@@ -0,0 +1,17 @@
+package com.songdan.demo.generics;
+
+/**
+ * @author Songdan
+ * @date 2016/4/26
+ */
+public class StackNoGeneric {
+
+    private Object[] elements;
+
+    public void push(Object o) {
+        /*
+        确保数组大小
+         */
+    }
+
+}
