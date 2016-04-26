fe70640fa958219397652509ee59480946e1e59d
sd1990 <songdan_obj@126.com>
2016-04-26 09:46:53 +0800

    add Generic

--- src/com/songdan/demo/generics/StackNoGeneric.java
+++ src/com/songdan/demo/generics/StackNoGeneric.java
@@ -1,5 +1,8 @@
 package com.songdan.demo.generics;
 
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
 /**
  * @author Songdan
  * @date 2016/4/26
@@ -8,10 +11,41 @@ public class StackNoGeneric {
 
     private Object[] elements;
 
+    private int size = 0;
+
+    private static final int DEFAULT_INITAL_CAPACITY = 16;
+
+    public StackNoGeneric() {
+        elements = new Object[DEFAULT_INITAL_CAPACITY];
+    }
+
+
     public void push(Object o) {
         /*
         确保数组大小
          */
+        ensureCapacity();
+        elements[size++] = o;
     }
+
+    private void ensureCapacity() {
+        if (elements.length==size) {
+            Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+
+    public Object pop() {
+        if (size==0) {
+            throw new EmptyStackException();
+        }
+        Object o = elements[--size];
+        elements[size] = null;
+        return o;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
 
 }
