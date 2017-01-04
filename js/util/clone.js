193ecaec72edc6f1bd79d84f986b585c98d5131d
sd1990 <songdan_obj@126.com>
2017-01-04 15:14:29 +0800

    deepclone

---
+++ js/util/clone.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Songdan on 2017/1/4.
+ */
+function objClone(obj) {
+    var newObject = {};
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            newObject[prop] = cloneProp(obj[prop]);
+        }
+    }
+    return newObject;
+}
+
+function cloneProp(value) {
+    var type = Object.prototype.toString.apply(value).slice(8, -1);
+    switch (type) {
+        case 'Number':
+        case 'String':
+            return value;
+        case 'Object':
+            return objClone(value);
+        case 'Array':
+            var array = [];
+            for (var i = 0; i < value.length; i++) {
+                array[i] = cloneProp(value[i]);
+            }
+            return array;
+    }
+}
